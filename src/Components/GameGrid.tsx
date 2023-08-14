220f50e4671b69846f74454163326e31ba5e132e
saahill007 <sgharat@buffalo.edu>
2023-08-13 22:07:09 -0500

    Added useGames hook

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -1,28 +1,10 @@
 import React, { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
 
-interface Game {
-  id: number;
-  name: string;
-}
-interface GamesResponse {
-  count: number;
-  results: Game[];
-}
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<GamesResponse>("/games")
-      .then((res) => {
-        setGames(res.data.results);
-        setError("");
-      })
-      .catch((err) => setError(err.message));
-  });
+  const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
