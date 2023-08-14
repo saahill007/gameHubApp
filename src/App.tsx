c72233a882069b50808671043d329f4caf9a61b8
saahill007 <sgharat@buffalo.edu>
2023-08-13 20:49:06 -0500

    grids created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area={"nav"} bg={"dodgerblue"}>
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area={"aside"} bg={"red"}>
+            Aside
+          </GridItem>
+        </Show>
+
+        <GridItem area={"main"} bg={"gold"}>
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
